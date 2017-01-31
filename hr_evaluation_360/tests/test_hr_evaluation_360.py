ba092e30124f71c8c9fe2a8c7b25bb8ab421e5d2
vonpupp <vonpupp@gmail.com>
2017-01-31 12:09:32 -0200

    Incremental commit before travel

--- hr_evaluation_360/tests/test_hr_evaluation_360.py
+++ hr_evaluation_360/tests/test_hr_evaluation_360.py
@@ -9,11 +9,10 @@ class TestHrEvaluation360(common.TransactionCase):
         super(TestHrEvaluation360, self).setUp(*args, **kwargs)
         cr, uid = self.cr, self.uid
 
-	import ipdb; ipdb.set_trace() # BREAKPOINT
         # Usefull models
         self.hr_employee = self.registry('hr.employee')
-        #self.hr_holidays = self.registry('hr.holidays')
-        #self.hr_holidays_status = self.registry('hr.holidays.status')
+        # self.hr_holidays = self.registry('hr.holidays')
+        # self.hr_holidays_status = self.registry('hr.holidays.status')
         self.res_users = self.registry('res.users')
         self.res_partner = self.registry('res.partner')
 
@@ -79,43 +78,70 @@ class TestHrEvaluation360(common.TransactionCase):
             'user_id': self.user_hruser_id,
         })
 
+    def test_01_anonymous_employee_self_appraisal_keeps_name(self):
+        """
+        On an anonymous 360 survey when an employee creates a self appraisal,
+        the name of the appraiser should be keeped
+        """
+        # user_vonpupp = self.env.ref('base.user_vonpupp')
+        # self.env = self.env(user=user_demo)
+        Survey = self.env['survey.survey']
+        Plan = self.env['hr_evaluation.plan']
+        Phase = self.env['hr_evaluation.plan.phase']
+        Evaluation = self.env['hr_evaluation.evaluation']
 
-        #cr, uid = self.cr, self.uid
-        #User = self.env['res.users'].with_context({'no_reset_password': True})
-        #import ipdb; ipdb.set_trace() # BREAKPOINT
-        #group_survey_user = self.registry('survey.hr.manager')
-        ##(group_survey_user, group_employee) = (self.ref('survey.group_survey_user'), self.ref('base.group_user'))
-        #self.survey_manager = User.create({
-        #    'name': 'Miléo',
-        #    'login': 'mileo',
-        #    'email': 'mileo@example.com',
-        #    #'groups_id': [(6, 0, [self.ref('survey.group_survey_manager'), group_survey_user, group_employee])]
-        #})
-        #self.survey_user = User.create({
-        #    'name': 'Vonpupp',
-        #    'login': 'vonpupp',
-        #    'email': 'vonpupp@example.com',
-        #    'groups_id': [(6, 0, [group_survey_user, group_employee])]
-        #})
-        #self.user_public = User.create({
-        #    'name': 'Public',
-        #    'login': 'public',
-        #    'email': 'public@example.com',
-        #    'groups_id': [(6, 0, [self.ref('base.group_public')])]
-        #})
-        #self.survey1 = self.env['survey.survey'].sudo(self.survey_manager).create({'title': "S0", 'page_ids': [(0, 0, {'title': "P0"})]})
-        #self.page1 = self.survey1.page_ids[0]
+        survey = Survey.create({
+            'title': 'survey title'
+        })
 
-    def test_01_create_employee_self_appraisal(self):
-        #user_vonpupp = self.env.ref('base.user_vonpupp')
-        #self.env = self.env(user=user_demo)
+        plan_360_anonymous = Plan.create({
+            'name': 'plan',
+            'company_id': '1',
+            'month_first': 3,
+            'month_next': 6,
+            'active': True,
+        })
+        import ipdb; ipdb.set_trace() # BREAKPOINT
+        #plan = Plan.search('name', 'ilike', '%Manager%')
+
+        send_phase = Phase.create({
+            'name': 'test_appraisal_plan',
+            'sequence': '1',
+            'company_id': '1',
+            'plan_id': plan_360_anonymous.id,
+            'action': '360-anonymous',
+            'survey_id': survey.id,
+            # 'survey_id': fields.many2one('survey.survey', 'Appraisal Form', required=True),
+            # 'send_answer_manager': fields.boolean('All Answers',
+            #     help="Send all answers to the manager"),
+            # 'send_answer_employee': fields.boolean('All Answers',
+            #     help="Send all answers to the employee"),
+            # 'send_anonymous_manager': fields.boolean('Anonymous Summary',
+            #     help="Send an anonymous summary to the manager"),
+            # 'send_anonymous_employee': fields.boolean('Anonymous Summary',
+            #     help="Send an anonymous summary to the employee"),
+            # 'wait': fields.boolean('Wait Previous Phases',
+            #     help="Check this box if you want to wait that all preceding phases " +
+            #       "are finished before launching this phase."),
+            # 'mail_feature': fields.boolean('Send mail for this phase', help="Check this box if you want to send mail to employees coming under this phase"),
+            # 'mail_body': fields.text('Email'),
+            # 'email_subject': fields.text('Subject')
+        })
+
+        evaluation = Evaluation.create({
+            'date': '2017-02-20',
+            'employee_id': self.employee_emp_id,
+            'plan_id': plan_360_anonymous.id,
+            #'state': 'done',
+        })
+        evaluation.button_plan_in_progress()
+        import ipdb; ipdb.set_trace() # BREAKPOINT
+
+        #self.assertEqual(task.is_done, False)
         pass
 
-    def create_appraisal_for_employee_gilles_gravie_by_employee_peiter_parker(self):
+    def test_02_anonymous_employee_appraisal_deletes_name(self):
         pass
 
-    def create_anonymous_appraisal_for_employee_gilles_gravie_by_employee_gilles_gravie(self):
-        pass
-
-    def create_anonymoous_appraisal_for_employee_gilles_gravie_by_employee_peiter_parker(self):
+    def test_03_anonymous_employee_apraisal_keeps_date(self):
         pass
